263c34af4790fdf63ec2db3bea9919d5a0354e7c
laphets <i@laphets.com>
2018-08-26 23:38:06 +0800

    Stable version

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        <nuxt/>
-    </div>
+    <v-app>
+        <div class="container">
+            <nuxt/>
+        </div>
+    </v-app>
 </template>
+
+<style lang="less">
+body {
+    background: #C9C9C9 !important;
+    font-family: 'Helvetica Neue',Helvetica,'Nimbus Sans L',Arial,'Liberation Sans','Hiragino Sans GB','Source Han Sans CN','Source Han Sans SC','Microsoft YaHei','Wenquanyi Micro Hei','WenQuanYi Zen Hei','ST Heiti',SimHei,'WenQuanYi Zen Hei Sharp',sans-serif !important;
+}
+.application--wrap {
+    background: #C9C9C9 !important;
+}
+html::before {
+    content: ' ';
+    display: block;
+    background-color: #4A4A4E;
+    width: 100%;
+    height: 25px;
+    position: relative;
+    z-index: 1;
+}
+.container {
+    padding-top: 0px !important;
+    max-width: 800px;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,484 @@
 <template>
-    <section class="container">
-        <v-layout>
-            <v-flex xs12 sm6 offset-sm3>
-            <v-card>
-                <v-card-title primary-title>
-                <div>
-                    <h3 class="headline mb-0">{{instance.name}}同学您好:</h3>
-                    <div>接下来希望你能够为你的每个志愿选择一个你认为合适的时间段参加面试~</div>
-                </div>
-                </v-card-title>
-            </v-card>
+    <div class="root">
+        <!-- error load-->
+        <v-snackbar
+        v-model="snackbar"
+        :multi-line="true"
+        :timeout="3000"
+        :bottom="true"
+        >
+        表单加载出现问题了呢~ 请刷新或确认地址无误后重试
+        <v-btn
+            color="pink"
+            flat
+            @click="snackbar = false"
+        >
+            关闭
+        </v-btn>
+        </v-snackbar>
 
-            <v-card v-for="(intent, index) in instance.intent_groups" :key="index">
-                <v-card-title primary-title>
-                <div>
-                    <h3 class="headline mb-0">{{intent.chinese_stage}}: {{intent.department}}</h3>
-                    <div>
-                        <v-radio-group @change="change">
-                        <v-radio
-                            color="primary"
-                            v-for="(interview, index1) in intent.interviews" :key="index1"
-                            :label="`${prase_time(interview.start_time)} ~ ${prase_time(interview.end_time)}`"
-                            :value="`${intent.intent_id}#${interview.ID}`"
-                        ></v-radio>
-                        </v-radio-group>
+
+        <!-- error submit -->
+        <v-snackbar
+        v-model="submiterror"
+        :multi-line="true"
+        :timeout="5000"
+        :top="true"
+        >
+        {{errorMsg}}
+        <v-btn
+            color="pink"
+            flat
+            @click="submiterror = false"
+        >
+            关闭
+        </v-btn>
+        </v-snackbar>
+
+        <!-- Loading -->
+        <v-dialog
+        v-model="loading"
+        hide-overlay
+        persistent
+        width="300"
+        >
+        <v-card
+            color="deep-purple"
+            dark
+        >
+            <v-card-text>
+            潮潮正在努力的为你提交报名表~
+            <v-progress-linear
+                indeterminate
+                color="white"
+                class="mb-0"
+            ></v-progress-linear>
+            </v-card-text>
+        </v-card>
+        </v-dialog>
+
+        <!-- Success Info -->
+<v-dialog v-model="submitted" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-card height="100%;">
+        <v-toolbar dark color="deep-purple">
+          <v-btn icon dark @click.native="submitted = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>表单提交成功</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark flat @click.native="submitted = false">关闭</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <div class="submitted-container">
+            <img src="~/assets/logo.svg" alt="head logo">
+            <div>
+                <h1>潮潮已经成功收到了你的表单呢~</h1>
+            </div>
+        </div>
+      </v-card>
+    </v-dialog>
+
+        <header>
+            <div class="header-container">
+                <h1>求是潮秋纳<br>报名表</h1>
+                <div class="sub-title">
+                    2018 QSC AUTUMN RECRUIT
+                </div>
+            </div>
+            <img src="~/assets/logo.svg" alt="head logo">
+        </header>
+
+        <main v-if="!error">
+            <!-- {{form}} -->
+            <v-form v-model="valid">
+                <div v-for="(item, index) in form" :key="item.tag">
+                    <div v-if="item.type === 'INPUT'">
+                        <v-text-field
+                        id="testing"
+                        name="input-1"
+                        :label="item.text"
+                        :rules="item.required? [v => !!v || '这个字段是必须的'] : []"
+                        v-model="data[item.tag]"
+                        ></v-text-field>
+                    </div>
+                    <div v-if="item.type === 'TEXT'">
+                        {{item.text}}
+                    </div>
+                    <div v-if="item.type === 'SELECT'">
+                        <div v-if="item.available_cnt === 1">
+                            <!-- only one -->
+                            <v-select
+                            :items="item.choices"
+                            v-model="data[item.tag]"
+                            :label="item.text"
+                            :rules="item.required? [v => !!v || '这个字段是必须的'] : []"
+                            item-value="tag"
+                            single-line
+                            ></v-select>
+                        </div>
+                        <div v-else>
+                            <!-- multi -->
+                            <div v-if="item.default_jump === false">
+                                <v-select
+                                :items="item.choices"
+                                v-model="data[item.tag]"
+                                :label="item.text"
+                                :rules="item.required? [v => !!v || '这个字段是必须的', v => v.length<=item.available_cnt || '选择了过多的选项'] : []"
+                                multiple
+                                max-height="400"
+                                item-value="tag"
+                                persistent-hint
+                                ></v-select>
+                                <div>
+                                    <!-- Render Choice -->
+                                    <div v-for="(cur, index) in item.choices" :key="index">
+                                        <div v-if=" data[item.tag] && data[item.tag].indexOf(cur.tag) != -1">
+                                            <div v-for="(subItem, index1) in cur.child">
+                                                <div v-if="subItem.type==='BOX'">
+                                                    <v-text-field
+                                                    id="testing"
+                                                    name="input-1"
+                                                    :label="`Box ${subItem.text}`"
+                                                    v-model="data[subItem.tag]"
+                                                    ></v-text-field>
+                                                </div>
+                                                <div v-if="subItem.type==='TEXTAREA'">
+                                                    <v-textarea
+                                                        v-model="data[subItem.tag]"
+                                                        auto-grow
+                                                        :label="subItem.text"
+                                                        rows="3"
+                                                    ></v-textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div v-else>
+
+                            </div>
+
                         </div>
                     </div>
+                    <div v-if="item.type==='UPLOAD'">
+                        {{item.text}} <input type="file" @change="fileSelected($event, item.tag)">
+                    </div>
+                    <div v-if="item.type==='BOX'">
+                        <v-text-field
+                        :rules="item.required? [v => !!v || '这个字段是必须的'] : []"
+                        v-model="data[item.tag]"
+                        id="testing"
+                        name="input-1"
+                        :label="`Box ${item.text}`"
+                        ></v-text-field>
+                    </div>
+                    <div v-if="item.type==='TEXTAREA'">
+                        <v-textarea
+                            :rules="item.required? [v => !!v || '这个字段是必须的'] : []"
+                            v-model="data[item.tag]"
+                            auto-grow
+                            :label="item.text"
+                            rows="3"
+                        ></v-textarea>
+                    </div>
                 </div>
-                </v-card-title>
-
-                <v-card-actions>
-                <v-btn @click="submit(intent.intent_id)" flat color="success">提交</v-btn>
-                <v-btn @click="reject(intent.intent_id)" flat color="error">拒绝</v-btn>
-                </v-card-actions>
-            </v-card>
-            </v-flex>
-        </v-layout>
-
-
-    </section>
+                <div class="submit">
+                    <button :disabled="!valid" :class="valid? '': 'disable'" @click.prevent="submit()">提交</button>
+                </div>
+            </v-form>
+        </main>
+        <footer>
+            <div>
+            报名如有问题请及时与我们(<a style="color: #9A9A9A" href="mailto:tech@zjuqsc.com">tech@zjuqsc.com</a>)联系
+            </div>
+            <p>©浙江大学求是潮 All rights reserved</p>
+            <p>
+            <a style="color: #64ACF9" href="https://www.qsc.zju.edu.cn" target="_blank">求是潮网站</a>
+            </p>
+        </footer>
+    </div>
 </template>
 
 <script>
-import axios from 'axios'
-import moment from 'moment';
-import 'moment/locale/zh-cn';
+import axios from "axios";
+import moment from "moment";
+import UploadButton from '../components/uploadBtn';
+
+const dfs = (rootTag, formMap) => {
+    const renderList = [];
+    const root = formMap.get(rootTag);
+    let next = root.next;
+    while (next != -1) {
+        const cur = next;
+        let curForm = formMap.get(cur);
+        next = curForm.next;
+        // console.log(curForm)
+        if (curForm.type === "SELECT" && !curForm.default_jump && curForm.choices.length ) {
+            // jump
+            //   console.log(curForm.choices);
+            for (let x in curForm.choices) {
+                let cur = curForm.choices[x];
+                const child = [];
+                let next1 = cur.next;
+                while (next1 != -1) {
+                    const curChild = next1;
+                    let curChildForm = formMap.get(curChild);
+                    next1 = curChildForm.next;
+                    child.push(curChildForm);
+                }
+                // console.log(child);
+                curForm.choices[x].child = child;
+            }
+        }
+        renderList.push(curForm);
+    }
+    return renderList
+};
+
+const waitFor1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 3000)
+    })
+}
 
 export default {
-    layout: 'schedule',
-    async asyncData (context) {
-        // console.log(context)
-        // console.log(context.query)
-        const { uid } = context.query
-        // console.log(this)
-        const { data } = ((await axios.get(`http://localhost:3000/v1/ssr/schedule?uid=${uid}`)).data)
-        return { instance: data }
+    components: {
+        UploadButton
+    },
+    // layout: 'schedule',    
+    async asyncData(context) {
+        try {
+            const { instanceId } = context.query
+            // console.log(instanceId)
+            const { data } = (await axios.get(
+                `http://localhost:3000/v1/ssr/form?instanceId=${instanceId}`
+            )).data;
+            const root_tag = data.root_tag;
+            let formList = (JSON.parse(data.data)).map((item) => {
+                const rule = []
+                if(item.required) {
+                    rule.push(v => !!v || '这个字段是必须的')
+                    // console.log(1)
+                }
+                // if(item.re) {
+                //     rule.push(v => (new RegExp(item.Re)).test(v) || '当前输入不符合')
+                //     // console.log(2)
+                // }
+                return {...item, rule: rule}
+            })
+            // console.log(formList)
+            let root = {};
+            const formMap = new Map();
+            formList.forEach((item, index) => {
+                formMap.set(item.tag, item);
+            });
+            // console.log(formMap)
+            // console.log(root_tag);
+
+            const renderList = dfs(root_tag, formMap);
+            // console.log(renderList[renderList.length - 1].choices[0].child);
+            // console.log()
+            return { form: renderList, snackbar: false, error: false };
+        } catch(error) {
+            console.log(error)
+            return {snackbar: true, error: true, form: []}
+        }
+
     },
     data() {
         return {
-            radioSelectList: ['1'],
-            a: 1,
-        }
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => v.length <= 10 || 'Name must be less than 10 characters'
+            ],
+            valid: false,
+            e1: "",
+            e6: "",
+            bio: "",
+            data: [],
+            submitted: false,
+            loading: false,
+            submiterror: false,
+            errorMsg: '',
+        };
     },
-    created() {
-
+    mounted() {
+        const instanceId = this.$route.query.instanceId
+        const store = window.localStorage.getItem(`instance${instanceId}`)
+        if(store) {
+            this.data = JSON.parse(store)
+        }
+        // this.data = window.
     },
     methods: {
-        prase_time(time) {
-            return moment(new Date(time)).format('LLL')
+        async fileSelected(e, tag) {
+            // console.log(e.target.files[0])
+            this.submitFile(e.target.files[0], tag)
         },
-        reject(intent_id) {
-            console.log(intent_id)
+        log() {
+            console.log(this.e6)
         },
-        async submit(intent_id) {
-            await axios.post(`http://localhost:3000/v1/ssr/join/${this.radioSelectList[intent_id]}`, {
-                intents: [intent_id]
-            })
-            // console.log(this.radioSelectList[intent_id])
+        async submitFile(file, tag) {
+            try {
+                const formData = new FormData()
+                formData.append('file', file)
+                const config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+                const res = await axios.post(`http://localhost:3000/v1/file/upload/img`, formData, config)
+                if (((res.data).code) != 0) {
+                    throw res.data
+                }
+                this.data[tag] = (res.data).data
+                console.log(this.data)
+                return
+            } catch (error) {
+                this.errorMsg = error.data
+                this.submiterror = true
+                return Promise.reject(error)
+            }
+
         },
-        change(e) {
-            const [intent_id, interview_id ]= (e.split('#')).map(item => ~~item)
-            this.radioSelectList[~~intent_id] = ~~interview_id
+        async submit() {
+            if(this.valid) {
+                try {
+                    this.loading = true
+                    const submitData = this.data.map((item) => {
+                        if(item instanceof Array) {
+                            // console.log('here', item)
+                            return item.map((it) => `${it}`)
+                        } else {
+                            // console.log('other', item)
+                            return [`${item}`]
+                        }
+                    })
+                    const real = []
+                    for(let x in submitData) {
+                        if(submitData[x]) {
+                            real.push({
+                                key: ~~x,
+                                value: submitData[x]
+                            })
+                        }
+                    }
+                    const instanceId = this.$route.query.instanceId
+                    const res = await axios.post(`http://localhost:3000/v1/freshman/submit?instanceId=${instanceId}`, {data: real})
+                    if (((res.data).code) != 0) {
+                        throw res.data
+                    }
+                    window.localStorage.setItem(`instance${instanceId}`, JSON.stringify(this.data))
+                    this.loading = false;
+                    this.submitted = true;
+                } catch (error) {
+                    this.loading = false;
+                    this.errorMsg = error.data
+                    this.submiterror = true
+                    // console.log(error.data)
+                }
+
+            }
+        }
+    }
+};
+</script>
+
+<style lang="less">
+.submitted-container {
+    img {
+        height: 200px;
+        width: 200px;
+    }
+    margin-top: 20vh;
+    vertical-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+header {
+    flex-grow: 1;
+    display: flex;
+    background-color: #FFFFFF;
+    box-shadow: #9a9a9a 0 0 20px 1px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    position: relative;
+    .header-container {
+        margin: 25px 50px;
+        h1 {
+            color: #212121;
+            text-align: left;
+            font-weight: 700;
+            font-size: 25px;
+            line-height: 1.3;
+            letter-spacing: 5px;
+            white-space: nowrap;
+        }
+        .sub-title {
+            font-size: 12px;
+            margin-top: 30px;
+            white-space: nowrap;
+        }
+    }
+    img {
+        width: 80px;
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+    }
+}
+main {
+    background-color: #FFFFFF;
+    box-shadow: #9a9a9a 0 0 20px 1px;
+    border-radius: 5px;
+    margin-top: 20px;
+    padding-top: 20px;
+    padding: 20px;
+    .submit {
+        margin-top: 30px;
+        display: flex;
+        justify-content: center;
+        button {
+            width: 175px;
+            height: 40px;
+            background: none;
+            border: #000 solid 1px;
+            border-radius: 5px;
+        }
+        .disable {
+            cursor: not-allowed;
+            border: rgba(128, 125, 125, 0.293) dashed 1px !important;
+            color: rgb(128, 125, 125, 0.093) !important;
         }
     }
 }
-</script>
+footer {
+    p {
+        margin-bottom: 0px;
+    }
+    text-align: center;
+    width: 100%;
+    color: #9A9A9A;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    font-size: 13px;
+    letter-spacing: 1px;
+    line-height: 22px;
+}
+// main::after {
+//     content: '';
+//     display: block;
+//     width: 100%;
+//     border-bottom-left-radius: 5px;
+//     border-bottom-right-radius: 5px;
+//     height: 20px;
+//     background: #D1E6FC;
 
-<style>
-.headline {
-    font-size: 22px !important;
-}
-.v-card {
-    margin-bottom: 20px;
-}
+// }
 </style>
